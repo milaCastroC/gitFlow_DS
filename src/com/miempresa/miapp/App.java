0a1d4f73c0b83a8b882cd67a953f2c7798e7e4d7
Mariluz Calderon <calderon.mariluz.8892@eam.edu.co>
2024-10-25 11:07:23 -0500

    ADD sumar y restar

--- src/com/miempresa/miapp/App.java
+++ src/com/miempresa/miapp/App.java
@@ -1,5 +1,11 @@
 package com.miempresa.miapp;
 
 public class App {
+	public double sumar(double a, double b) {
+		return a + b;
+	}
 
+	public double restar(double a, double b) {
+		return a - b;
+	}
 }
