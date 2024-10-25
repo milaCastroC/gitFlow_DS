acd0c4726a7c452690e14aeab35568def06942e1
Maria Camila Castro Cardona <castro.maria.5191@eam.edu.co>
2024-10-25 11:27:12 -0500

    ADD multiplicar y dividir

--- src/com/miempresa/miapp/AppCamila.java
+++ src/com/miempresa/miapp/AppCamila.java
@@ -1,5 +1,19 @@
 package com.miempresa.miapp;
 
 public class AppCamila {
+	public static double sumar(double a, double b) {
+		return a + b;
+	}
 
+	public static double restar(double a, double b) {
+		return a - b;
+	}
+	
+	public static double multiplicar(double a, double b) {
+		return a * b;
+	}
+	
+	public static double dividir(double a, double b) {
+		return a / b;
+	}
 }
