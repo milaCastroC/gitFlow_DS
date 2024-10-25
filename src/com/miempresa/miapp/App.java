9a146cc5630522e33d6edc2a0f857ca67542396a
Mariluz Calderon <calderon.mariluz.8892@eam.edu.co>
2024-10-25 11:10:31 -0500

    ADD multiplicar y dividir

--- src/com/miempresa/miapp/App.java
+++ src/com/miempresa/miapp/App.java
@@ -1,11 +1,19 @@
 package com.miempresa.miapp;
 
 public class App {
-	public double sumar(double a, double b) {
+	public static double sumar(double a, double b) {
 		return a + b;
 	}
 
-	public double restar(double a, double b) {
+	public static double restar(double a, double b) {
 		return a - b;
+	}
+	
+	public static double multiplicar(double a, double b) {
+		return a * b;
+	}
+	
+	public static double dividir(double a, double b) {
+		return a / b;
 	}
 }
